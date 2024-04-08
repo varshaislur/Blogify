1b3a7425ce140ca35404b5cd9a36cb90300b662f
varshaislur <varshaislur@gmail.com>
2024-04-09 00:06:07 +0530

    added blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,18 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { blogList } from '../../config/data';
+import './Blog.css'
+import Chip from '../../components/Chip/Chip';
 
 const Blog = () => {
  const {id}= useParams();
  const [blog,setBlog]=useState(null);
  useEffect(()=>{
   const newblog=blogList.find(blog=>blog.id===parseInt(id));
-  if(blog){
-    setBlog(newblog)
+  if(newblog){
+    setBlog(newblog);
   }
- },[blog]);
+ },[id]);
   return (
-    <div>Blog</div>
+    <div className='blog-wrap'>
+
+
+   
+   {blog ?(
+   <div>
+    <header >
+      {blog.title}
+    </header>
+    <span>
+    {blog.subCategory.map((subCategory,index)=><div><Chip key={index} label={subCategory} /></div>)}
+    </span>
+    <span>
+    <img src={blog.cover} />
+    </span>
+    <p>
+      {blog.description}
+    </p>
+   
+   </div>)
+   : null}
+    </div>
+    
   )
 }
 
